9b46983940141460404a2204aa2dbbbd725a4723
whipowill <whipowill@protonmail.com>
2023-07-30 23:51:00 -0600

    Updates.

--- src/ModDruidBuff.cpp
+++ src/ModDruidBuff.cpp
@@ -9,7 +9,7 @@ class DruidBuff : public PlayerScript
 public:
     DruidBuff() : PlayerScript("DruidBuff") {}
 
-    void OnUpdate(Player* player)
+    void OnUpdate(Player* player) override
     {
         int is_enabled = sConfigMgr->GetOption<int>("DruidBuff.Enable", 0);
 
@@ -17,28 +17,31 @@ public:
         {
             if (player)
             {
-                // from wowhead
-                uint32_t aura_savage_roar = 52610;
-                uint32_t aura_cat_form = 768;
-                uint32_t aura_bear_form = 5487;
-                uint32_t aura_dire_bear_form = 9634;
-
-                if (player->getClass() == CLASS_DRUID && (player->HasAura(aura_cat_form) || player->HasAura(aura_bear_form) || player->HasAura(aura_dire_bear_form)))
+                if (player->getClass() == CLASS_DRUID)
                 {
-                    if (!player->HasAura(aura_savage_roar))
-                    {
-                        int buffDurationSeconds = 60 * 60;
-                        uint32_t buffDurationMillis = buffDurationSeconds * 1000;
+                    // from wowhead
+                    uint32_t aura_savage_roar = 24425; //52610;
+                    uint32_t aura_cat_form = 768;
+                    uint32_t aura_bear_form = 5487;
+                    uint32_t aura_dire_bear_form = 9634;
 
-                        player->AddAura(aura_savage_roar, player);
-                        player->GetAura(aura_savage_roar, player->GetGUID())->SetDuration(buffDurationMillis);
+                    if (player->HasAura(aura_cat_form) || player->HasAura(aura_bear_form) || player->HasAura(aura_dire_bear_form))
+                    {
+                        if (!player->HasAura(aura_savage_roar))
+                        {
+                            int buffDurationSeconds = 60 * 60;
+                            uint32_t buffDurationMillis = buffDurationSeconds * 1000;
+
+                            player->AddAura(aura_savage_roar, player);
+                            //player->GetAura(aura_savage_roar, player->GetGUID())->SetDuration(buffDurationMillis);
+                        }
                     }
-                }
-                else
-                {
-                    if (player->HasAura(aura_savage_roar))
+                    else
                     {
-                        player->RemoveAura(aura_savage_roar);
+                        if (player->HasAura(aura_savage_roar))
+                        {
+                            player->RemoveAura(aura_savage_roar);
+                        }
                     }
                 }
             }
