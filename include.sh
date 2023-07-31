59235ff1f5c2a850c388837aa13a8b601e28f5fc
whipowill <whipowill@protonmail.com>
2023-07-31 16:38:39 -0600

    Updates.

--- include.sh
+++ include.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 ## GETS THE CURRENT MODULE ROOT DIRECTORY
-MOD_DRUIDBUFF_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+MOD_SKELETON_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
-source $MOD_DRUIDBUFF_ROOT"/conf/conf.sh.dist"
+source $MOD_SKELETON_ROOT"/conf/conf.sh.dist"
 
-if [ -f $MOD_DRUIDBUFF_ROOT"/conf/conf.sh" ]; then
-    source $MOD_DRUIDBUFF_ROOT"/conf/conf.sh"
+if [ -f $MOD_SKELETON_ROOT"/conf/conf.sh" ]; then
+    source $MOD_SKELETON_ROOT"/conf/conf.sh"
 fi
