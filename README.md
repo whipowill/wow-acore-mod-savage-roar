c953ceadc5bcd0baf843e91b91d03527823c15a9
whipowill <whipowill@protonmail.com>
2023-07-30 20:36:23 -0600

    Init.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Savage Roar
+
+An AzerothCore module for giving Druid's the love they have long deserved.
+
+While all the private servers a infested w/ over-powered Paladins, those of us who actually love the Druid class continue to get shafted by 20 years of developer blindness.  This module seeks to rectify that mistake on my private server by giving Druids a passive Savage Roar buff while shapeshifted, granting a totally legit and balancing +30% increase to damage.
+
+This is my first attempt at creating a module.
---
+++ src/ModDruidBuff.cpp
@@ -0,0 +1,47 @@
+#include "Player.h"
+#include "Unit.h"
+#include "Config.h"
+
+class DruidBuff : public PlayerScript
+{
+public:
+    DruidBuff() : Module("DruidBuff") {}
+
+    void OnUpdate(Player* player)
+    {
+        int is_enabled = sConfigMgr->GetIntDefault("DruidBuff.Enable", 0);
+
+        if (is_enabled)
+        {
+            uint32_t spellId = 52610; // Savage Roar
+
+            // if in animal form
+            if (player && player->IsDruid() && (player->HasAura(AURA_CAT_FORM) || player->HasAura(AURA_BEAR_FORM) || player->HasAura(AURA_DIRE_BEAR_FORM)))
+            {
+                if (!player->HasAura(spellId))
+                {
+                    // Apply the damage increase buff
+                    int damageIncreasePct = sConfigMgr->GetIntDefault("DruidBuff.Modifier", 30); // Get the value from the config
+                    player->ModifyDamage(damageIncreasePct); // Modify the player's damage
+
+                    int buffDuration = 60 * 60 * 6; // 5 hours (adjust as needed)
+
+                    player->AddAura(spellId, player); // Apply the buff to the player
+                    player->SetAuraDuration(spellId, buffDuration); // Set the duration of the buff
+                }
+            }
+            else
+            {
+                if (player->HasAura(spellId))
+                {
+                    player->RemoveAura(spellId);
+                }
+            }
+        }
+    }
+};
+
+void AddDruidBuffScripts()
+{
+    new DruidBuff();
+}
